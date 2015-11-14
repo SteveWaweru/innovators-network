8332b4ea910dbdc1b6f90e5ba83d03728e9003d9
steve <steviewez@gmail.com>
2015-11-14 14:25:58 +0300

    catch duplicate emails

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Mail(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     def __unicode__(self):
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 from django.views.generic import View
 from .models import Mail
@@ -5,13 +6,15 @@ from .models import Mail
 
 class Home(View):
     template_name = 'dashboard/index.html'
+    message = 'Your Email has been saved successfully. Thank you for your subscription.'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
-        user_email = Mail.objects.create()
-        user_email.email = request.POST.get('email')
-        user_email.save()
-        return render(request, self.template_name, {'success': 'Your Email has been saved successfully. Thank you for '
-                                                               'your subscription. '})
+        try:
+            user_email = Mail.objects.create(email=request.POST.get('email'))
+            user_email.save()
+        except IntegrityError as e:
+            self.message = 'We envy your excitement. Thank you, but we already have your mail with us.'
+        return render(request, self.template_name, {'success': self.message })
